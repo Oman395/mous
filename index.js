071a1232b5866fe0624fb24266754a81fc51ef7c
Oran Hartley <oranroha@gmail.com>
2021-10-01 14:14:03 -0400

    2 levels support

--- index.js
+++ index.js
@@ -56,22 +56,6 @@ function sort(dta, opts, callback) {
         }
     });
 }
-
-function saveData(dta, extfolder) {
-    Object.keys(dta).forEach((item) => {
-        if (!fs.existsSync(`Output(${file})`)) fs.mkdirSync(`Output(${file})`);
-        if (!fs.existsSync(`Output(${file})/${extfolder}/`)) fs.mkdirSync(`Output(${file})/${extfolder}/`)
-        if (!fs.existsSync(`Output(${file})/${extfolder}/${item.replace('\r', '')}`)) fs.mkdirSync(`Output(${file})/${extfolder}/${item.replace('\r', '')}`);
-        Object.keys(dta[item]).forEach((thing) => {
-            var str = "";
-            Object.keys(dta[item][thing]).forEach((that) => {
-                str += `${that}: ${dta[item][thing][that]}\n`;
-            })
-            fs.writeFileSync(`Output(${file})/${extfolder}/${item.replace('\r', '')}/${thing.replace('\r', '')}.txt`, str);
-        });
-    })
-}
-
 function start() {
     getFile((dta) => {
         data = dta;
@@ -85,27 +69,87 @@ function start() {
     });
 }
 start();
-
+function sorttoo(args) {
+    var returnedData = {};
+    if (args.length > 1) {
+        args.forEach((arg, index) => {
+            if (index == 0) {
+                Object.keys(parsed).forEach((label) => {
+                    if (!returnedData[parsed[label][arg]]) returnedData[parsed[label][arg]] = {};
+                })
+            } else {
+                Object.keys(returnedData).forEach((loc) => {
+                    Object.keys(parsed).forEach((label) => {
+                        if (parsed[label][args[0]] == loc) {
+                            if (!returnedData[loc][parsed[label][arg]]) returnedData[loc][parsed[label][arg]] = {};
+                            returnedData[loc][parsed[label][arg]][label] = {};
+                            Object.keys(parsed[label]).forEach((entry) => {
+                                if (!args.includes(entry)) {
+                                    returnedData[loc][parsed[label][arg]][label][entry] = parsed[label][entry];
+                                }
+                            });
+                        }
+                    });
+                });
+            }
+        })
+    } else {
+        args.forEach((arg, index) => {
+            Object.keys(parsed).forEach((label) => {
+                if (!returnedData[parsed[label][arg]]) returnedData[parsed[label][arg]] = {};
+                returnedData[parsed[label][arg]][label] = {};
+                Object.keys(parsed[label]).forEach((entry) => {
+                    if (!args.includes(entry)) {
+                        returnedData[parsed[label][arg]][label][entry] = parsed[label][entry];
+                    }
+                });
+            });
+        });
+    }
+    return (returnedData);
+}
+function write(returnedData, args) {
+    if (args.length > 1) {
+        Object.keys(returnedData).forEach((one) => {
+            if (!fs.existsSync(`Output(${file}, ${args[0]}-${args[1]})`)) fs.mkdirSync(`Output(${file}, ${args[0]}-${args[1]})`);
+            if (!fs.existsSync(`Output(${file}, ${args[0]}-${args[1]})/${one.replace("\r", '')}`)) fs.mkdirSync(`Output(${file}, ${args[0]}-${args[1]})/${one.replace("\r", '')}`);
+            var path = `Output(${file}, ${args[0]}-${args[1]})/${one.replace("\r", '')}`;
+            Object.keys(returnedData[one]).forEach((two) => {
+                if (!fs.existsSync(`${path}/${two.replace("\r", '')}`)) fs.mkdirSync(`${path}/${two.replace("\r", '')}`);
+                Object.keys(returnedData[one][two]).forEach((item) => {
+                    var str = "";
+                    Object.keys(returnedData[one][two][item]).forEach((entry) => {
+                        str += `${entry}: ${returnedData[one][two][item][entry]}\n`;
+                    })
+                    fs.writeFileSync(`${path}/${two.replace("\r", '')}/${item.replace('\r', '')}.txt`, str);
+                })
+            })
+        })
+    } else {
+        if (!fs.existsSync(`Output(${file}, ${args[0]})`)) fs.mkdirSync(`Output(${file}, ${args[0]}})`);
+        var path = `Output(${file}, ${args[0]}})`;
+        Object.keys(returnedData).forEach((one) => {
+            if (!fs.existsSync(`${path}/${one.replace("\r", '')}`)) fs.mkdirSync(`${path}/${one.replace("\r", '')}`);
+            Object.keys(returnedData[one]).forEach((item) => {
+                var str = "";
+                Object.keys(returnedData[one][item]).forEach((entry) => {
+                    str += `${entry}: ${returnedData[one][item][entry]}\n`;
+                })
+                fs.writeFileSync(`${path}/${one.replace("\r", '')}/${item.replace('\r', '')}.txt`, str);
+            })
+        })
+    }
+}
 function loop() {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
     rl.question('What would you like to sort by? Send "e" to exit. ', (answer) => {
-        if (answer != 'e' && answer != 'E') {
-            var response = {};
-            Object.keys(parsed).forEach((parse) => {
-                if (parsed[parse]) {
-                    if (!response[parsed[parse][answer]]) response[parsed[parse][answer]] = {};
-                    response[parsed[parse][answer]][parse] = {};
-                    Object.keys(parsed[parse]).forEach((item) => {
-                        if (answer != item) {
-                            response[parsed[parse][answer]][parse][item] = parsed[parse][item];
-                        }
-                    })
-                }
-            });
-            saveData(response, answer);
+        if (answer != "E" && answer != "e") {
+            var args = answer.split(" ");
+            var returnedData = sorttoo(args);
+            write(returnedData, args);
             rl.close();
             loop();
         } else {
